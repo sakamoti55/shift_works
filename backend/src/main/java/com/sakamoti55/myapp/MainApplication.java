9062a77f043d07eff443138371e2e99ca571c32e
sakamoti55 <kohseimeimei@gmail.com>
2024-12-28 18:07:50 +0900

    MainApplication.javaの作成

---
+++ backend/src/main/java/com/sakamoti55/myapp/MainApplication.java
@@ -0,0 +1,13 @@
+package com.sakamoti55.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sakamoti55.myapp.MainApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
