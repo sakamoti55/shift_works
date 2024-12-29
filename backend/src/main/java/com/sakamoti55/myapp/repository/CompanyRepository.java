e5d066d3a5e3539b4326fcde4df5228c4235696e
sakamoti55 <kohseimeimei@gmail.com>
2024-12-29 19:38:13 +0900

    companyNameをkeyにデータを取得するクエリの作成

--- backend/src/main/java/com/sakamoti55/myapp/repository/CompanyRepository.java
+++ backend/src/main/java/com/sakamoti55/myapp/repository/CompanyRepository.java
@@ -1,9 +1,19 @@
 package com.sakamoti55.myapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.sakamoti55.myapp.entity.Company;
+
+// import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    // companyNameをキーにデータを取得
+    // :companyNameはプレースホルダー
+    @Query("SELECT c FROM Company c WHERE c.companyName = :companyName")
+    Optional<Company> findByCompanyName(String companyName);
 }
