70914ed129f86612e7a3d14e827939175d22457b
sakamoti55 <kohseimeimei@gmail.com>
2024-12-30 00:22:47 +0900

    formの作成

--- frontend/src/components/syain_comp/s_login_comp/ShiftRegister.jsx
+++ frontend/src/components/syain_comp/s_login_comp/ShiftRegister.jsx
@@ -1,7 +1,56 @@
+import React, { useState, useEffect } from 'react';
+
 function ShiftRegister(){
-    
+    const [employeeId,setEmployeeId] = useState();
+    const [date,setDate] = useState();
+    const [startTime,setStartTime] = useState();
+    const [endTime,setEndTime] = useState();
+
+    const [message,setMessage] = useState('');
+    const handleClick = async (e) => {
+        }
     return (
+        <div>
             <h2>ShiftRegister</h2>
+            <div>
+                <form>
+                    <div>
+                    <label>社員ID: </label>
+                    <input 
+                        type="number"
+                        value={employeeId}
+                        onChange={e => setEmployeeId(e.target.value)} 
+                    />
+                    </div>
+                    <div>
+                    <label>日付: </label>
+                    <input
+                        type="date"
+                        value={date}
+                        onChange={e => setDate(e.target.value)}
+                    />
+                    </div>
+                    <div>
+                    <label>開始時刻: </label>
+                    <input
+                        type="time"
+                        value={startTime}
+                        onChange={e => setStartTime(e.target.value)}
+                    />
+                    </div>
+                    <div>
+                    <label>終了時刻: </label>
+                    <input
+                        type="time"
+                        value={endTime}
+                        onChange={e => setEndTime(e.target.value)}
+                    />
+                    </div>
+                    <button onSubmit={handleClick}>register</button>
+                </form>
+                {message ? <p>{message}</p> : <p>nonono</p>}
+            </div>
+        </div>
     )
 }
 
