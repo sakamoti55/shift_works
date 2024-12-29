7fd3a8950eba031fcb0bad8745fbf7d8fffe2384
sakamoti55 <kohseimeimei@gmail.com>
2024-12-29 19:44:32 +0900

    getCompanyId関数の処理を作成

--- frontend/src/components/houjin_comp/HRegister.jsx
+++ frontend/src/components/houjin_comp/HRegister.jsx
@@ -2,9 +2,41 @@ import React, { useState, useEffect } from 'react';
 
 function HRegister(){
 
-    const [companyName,setCompanyName] = useState();
-    const [message,setMessage] = useState();
+    const [companyName,setCompanyName] = useState('');
+    const [message,setMessage] = useState('');
+    const [companyId,setCompanyId] = useState('');
+    const [message2,setMessage2] = useState('');
 
+    const getCompanyId = async() => {
+
+        try {
+            // 非同期
+            const res = await fetch(`http://localhost:8080/api/company/${companyName}`,{
+                method: 'GET',
+                headers:{'Content-Type': 'application/json'}
+            });
+
+            if(!res.ok){
+                throw new Error('Failed to fetch companyId');
+            }
+
+            // 非同期
+            const json = await res.json();
+            
+            // setMessage2(`your company id is ${json.companyId}.`);
+
+            setCompanyId(json.companyId);
+
+        }catch{
+            console.log(err);
+            setMessage2('Fetching companyId failed');
+        }
+    }
+
+    // 中身の処理を書きやすいのでuseEffectで書き直した
+    useEffect(() => {
+        companyId === '' ? setMessage2(`Company ID will be displayed here.`) : setMessage2(`your company id is ${companyId}.`) 
+    },[companyId])
 
     // form送信時の挙動
     const handelSubmit = async(e) => {
