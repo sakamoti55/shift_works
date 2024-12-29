92d49d6100e84382717393979fb76948d2bd2b3d
sakamoti55 <kohseimeimei@gmail.com>
2024-12-29 23:10:59 +0900

    employeeNameをキーにデータを取得クエリを作成

--- backend/src/main/java/com/sakamoti55/myapp/repository/EmployeeRepository.java
+++ backend/src/main/java/com/sakamoti55/myapp/repository/EmployeeRepository.java
@@ -1,9 +1,17 @@
 package com.sakamoti55.myapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.sakamoti55.myapp.entity.Employee;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // employeeNameをキーにデータを取得
+    // :employeeNameはプレースホルダー
+    @Query("SELECT e FROM Employee e WHERE e.employeeName = :employeeName")
+    Optional<Employee> findByEmployeeName(String employeeName);    
 }
