cfa0d9e947bea313e105b3173c45abaf04fc76ea
sakamoti55 <kohseimeimei@gmail.com>
2024-12-29 20:30:18 +0900

    formの作成

--- frontend/src/components/houjin_comp/h_login_comp/SRegister.jsx
+++ frontend/src/components/houjin_comp/h_login_comp/SRegister.jsx
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
 function SRegister(){
-    
+    const [companyId,setCompanyId] = useState('');
+    const [employeeName,setEmployeeName] = useState('');
+
+    // 削除すること
+    // useEffect(() => {
+    //     console.log(companyId);
+    //     console.log(employeeName);
+    // },[companyId,employeeName])
     return (
+        <div>
             <h2>SRegister</h2>
+            <div>
+                <form>
+                    <div>
+                    <label>会社ID: </label>
+                    <input
+                        type="number"
+                        value={companyId}
+                        onChange={e => setCompanyId(e.target.value)}
+                    />
+                    </div>
+                    <div>
+                    <label>社員名: </label>
+                    <input
+                        type="text"
+                        value={employeeName}
+                        onChange={e => setEmployeeName(e.target.value)}
+                    />
+                    </div>
+                    <button type="submit">register</button>
+                </form>
+            </div>
+        </div>
+
     )
 }
 
